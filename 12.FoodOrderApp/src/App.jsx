85ad973eb87d5886cec4d2396297a50ba29c3f72
Shyam <gshyamprasad2001@gmail.com>
2025-02-06 08:37:32 +0530

    added checkout

--- 12.FoodOrderApp/src/App.jsx
+++ 12.FoodOrderApp/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import { CartContextProvider } from "./store/CartContext";
 import { UserProgressContextProvider } from "./store/UserProgressContext";
 import Cart from "./components/Cart";
+import CheckOut from "./components/CheckOut";
 
 
 
@@ -14,6 +15,7 @@ function App() {
           <Navbar />
           <Meals />
           <Cart />
+          <CheckOut />
         </CartContextProvider>
       </UserProgressContextProvider>
     </>
